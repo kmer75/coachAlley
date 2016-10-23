405e5d5ebe7b3e339399ec7da3a4e8cd5d6bf23c
DESKTOP-721TNL2\kevin <kevinngakam@gmail.com>
2016-10-23 20:47:19 +0200

    validation formulaire terminé, mise en place d'un template bootstrap pour le header, et les differentes route accessible via l'interface du site. Le projet de mise en place d'une base de travail est fini

--- src/main/java/com/example/controllers/TestController.java
+++ src/main/java/com/example/controllers/TestController.java
@@ -6,11 +6,13 @@ import com.example.entities.User;
 import com.example.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.mail.MessagingException;
 import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 
 /**
  * Created by kevin on 12/10/2016.
@@ -25,7 +27,9 @@ public class TestController {
     EmailSenderServiceImpl emailSender;
 
     @RequestMapping(value = "/index", method = RequestMethod.GET)
-    public String index() {
+    public String index(Model model, Principal principal) {
+        String name = principal.getName();
+        if(name != null) model.addAttribute("username", name);
         return "index";
     }
 
